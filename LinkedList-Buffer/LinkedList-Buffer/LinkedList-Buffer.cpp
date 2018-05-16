03e3e6c0512ff7e1dbb459228a6d57381e52d4d2
Andrew Durkiewicz <andrewdurkiewicz@gmail.com>
2018-05-16 14:56:08 -0700

    My pointers are a mess. will fix later tonight

--- LinkedList-Buffer/LinkedList-Buffer/LinkedList-Buffer.cpp
+++ LinkedList-Buffer/LinkedList-Buffer/LinkedList-Buffer.cpp
@@ -2,24 +2,35 @@
 #include <fstream>
 #include <string>
 using namespace std;
-typedef struct Node {
+
+typedef struct{
 	string firstName;
 	string lastName;
 	double gpa;
-	Node* next;
-};
+	Node* next = NULL;
+}Node;
+
+Node* initNode(string first, string last);
+void setNext(Node* thisPtr, Node* nextPtr);
 
 int main()
 {
 	string text = "";
 	ifstream openfile("Name.txt");
+	static int count = 0;
 	if (openfile.is_open())
 	{
 		while(!openfile.eof())
 		{
+
 			string nameValue;
 			openfile >> nameValue;
-			text += nameValue;
+			string firstName = nameValue;
+			string lastName = nameValue;
+			Node* lastPtr = initNode(firstName, lastName);
+			count++;
+
+			
 		}
 
 	}
@@ -27,4 +38,18 @@ int main()
 	{
 	}
 	return 0;
+}
+
+Node* initNode(string first, string last)
+{
+	Node* tmp = new Node();
+	tmp->firstName = first;
+	tmp->lastName = last;
+	tmp->next = NULL;
+	return tmp;
+}
+
+void setNext(Node* thisPtr, Node* nextPtr)
+{
+	thisPtr->next = nextPtr;
 }
