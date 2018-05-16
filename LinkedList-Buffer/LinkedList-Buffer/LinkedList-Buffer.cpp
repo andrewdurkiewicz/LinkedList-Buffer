b4833088880df741431b9580d2595ccd6c67c068
Andrew Durkiewicz <andrewdurkiewicz@gmail.com>
2018-05-16 16:55:04 -0700

    update before going home

--- LinkedList-Buffer/LinkedList-Buffer/LinkedList-Buffer.cpp
+++ LinkedList-Buffer/LinkedList-Buffer/LinkedList-Buffer.cpp
@@ -3,12 +3,12 @@
 #include <string>
 using namespace std;
 
-typedef struct{
+typedef struct Node{
 	string firstName;
 	string lastName;
 	double gpa;
-	Node* next = NULL;
-}Node;
+	Node* next;
+};
 
 Node* initNode(string first, string last);
 void setNext(Node* thisPtr, Node* nextPtr);
@@ -17,7 +17,9 @@ int main()
 {
 	string text = "";
 	ifstream openfile("Name.txt");
-	static int count = 0;
+	Node* tempPtr;
+	Node* head;
+	int count = 0;
 	if (openfile.is_open())
 	{
 		while(!openfile.eof())
@@ -26,17 +28,28 @@ int main()
 			string nameValue;
 			openfile >> nameValue;
 			string firstName = nameValue;
+			openfile >> nameValue;
 			string lastName = nameValue;
-			Node* lastPtr = initNode(firstName, lastName);
-			count++;
+			tempPtr->next = newPtr;
+			Node* newPtr = initNode(firstName, lastName);
+			tempPtr = newPtr;
 
-			
+			if (count == 0)
+			{
+				head = newPtr;
+				
+			}
+			tempPtr = newPtr;
+			count++;
+			system("pause");
+
 		}
 
 	}
 	else
 	{
 	}
+	system("pause");
 	return 0;
 }
 
@@ -45,7 +58,6 @@ Node* initNode(string first, string last)
 	Node* tmp = new Node();
 	tmp->firstName = first;
 	tmp->lastName = last;
-	tmp->next = NULL;
 	return tmp;
 }
 
